e36f7344d564d21f133889a73766ac91143ccefb
Arthur Martins <arthur.mezaonik@gmail.com>
2026-02-01 21:56:18 +0000

    Configuracao do Dockerfile + criacao do application-docker

--- src/main/java/br/com/fiap/movimentacao_service/entities/record/response/MovimentacaoRecordResponse.java
+++ src/main/java/br/com/fiap/movimentacao_service/entities/record/response/MovimentacaoRecordResponse.java
@@ -1,9 +1,6 @@
 package br.com.fiap.movimentacao_service.entities.record.response;
 
 import br.com.fiap.movimentacao_service.entities.enums.TipoMovimentacao;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
